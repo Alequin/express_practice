7c8d75658a12126c2966acbebee3e030d5987ca8
James Cox <j-cox03@hotmail.com>
2017-08-07 23:04:26 +0100

    Connected controllers

--- controllers/films.js
+++ controllers/films.js
@@ -1,4 +1,12 @@
 //since we don't have a database we'll use our front end models at the moment
+var express = require('express');
+var filmRouter = new express.Router();
 var films = require('../client/src/models/films')();
 var Film = require('../client/src/models/film');
 var Review = require('../client/src/models/review');
+
+filmRouter.get('/film', function (req, res) {
+
+});
+
+module.exports = filmRouter;
--- server.js
+++ server.js
@@ -9,3 +9,5 @@ app.use(express.static('client/build'));
 app.listen(3000, function () {
   console.log('App running on port '+this.address().port);
 });
+
+app.use(require("./controllers/index.js"));
